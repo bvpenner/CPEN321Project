fa8cf057de5bb89bae4758350bb86f3a38901987
Misty Shadows <misty@mistyshadows.horse>
2025-02-26 03:25:49 -0800

    Attempting to squash bugs related to MainActivity restarting when not desired

--- app/app/src/main/java/com/example/cpen321app/AddTask.kt
+++ app/app/src/main/java/com/example/cpen321app/AddTask.kt
@@ -1,12 +1,19 @@
 package com.example.cpen321app
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 
 class AddTask : AppCompatActivity() {
+
+    private lateinit var taskViewModel: TaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,31 @@ class AddTask : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val buttonCreate = findViewById<Button>(R.id.button_taskCreate)
+
+        buttonCreate.setOnClickListener {
+
+            val id: String = "Placeholder"
+            val name: String = findViewById<EditText>(R.id.taskName).text.toString()
+            val start: String = findViewById<EditText>(R.id.taskStart).text.toString()
+            val end: Int = findViewById<EditText>(R.id.taskEnd).text.toString().toInt()
+            val location_lat: Double = findViewById<EditText>(R.id.taskLat).text.toString().toDouble()
+            val location_lng: Double = findViewById<EditText>(R.id.taskLng).text.toString().toDouble()
+            val priority: Int = findViewById<EditText>(R.id.taskPriority).text.toString().toInt()
+            val description: String = findViewById<EditText>(R.id.taskDescription).text.toString()
+
+            val newTask = Task(id, name, start, end, location_lat, location_lng, priority, description)
+
+            // Send to backend and then update the list with the new task including the id.
+
+            // temp code to add to list
+//            taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+//
+//            taskViewModel.addTask(newTask)
+
+            finish()
         }
 
     }
